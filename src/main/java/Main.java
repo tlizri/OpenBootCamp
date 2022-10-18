21f79b46a134b4d64480ebf766c03077bb1609d6
tlizri <tlizri@outlook.com>
2022-10-18 12:33:45 +0200

    Change scripts to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import resource.Ejercicio3;
 
 public class Main {
     public static void main(String[] args) {
-        Ejercicio3.main();
+        System.out.println("Parte1: Sumar");
+        System.out.println(Ejercicio3.sumar(1.24f, 2.98f, 3.44f));
+        System.out.println("Parte2: Clase coche");
+        System.out.println(Ejercicio3.incrementarNumPuertas());
     }
 }
--- src/main/java/resource/Ejercicio3.java
+++ src/main/java/resource/Ejercicio3.java
@@ -1,17 +1,13 @@
 package resource;
 
 public class Ejercicio3 {
-    public static void main(){
-        //Parte1
-        float resultado = sumar(1.24f, 2.98f, 3.44f);
-        System.out.println(resultado);
-        //Parte2
-        Coche miCoche = new Coche();
-        miCoche.IncrementoPuertas(1);
-        System.out.println(miCoche.puertas);
-    }
     public static float sumar(float a, float b, float c) {
         return a + b + c;
+    }
+    public static int incrementarNumPuertas(){
+        Coche miCoche = new Coche();
+        miCoche.IncrementoPuertas(1);
+        return miCoche.puertas;
     }
 }
 
